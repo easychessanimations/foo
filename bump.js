b05029edd51a50b96ecc176ae1a409b54271f302
easychessanimations <chessbotroulette@gmail.com>
2020-10-23 03:43:55 +0000

    Web

--- bump.js
+++ bump.js
@@ -11,3 +11,11 @@ packagejson.version = newversion
 fs.writeFileSync("package.json", JSON.stringify(packagejson, null, 2))
 
 console.log("bumped", newversion)
+
+let foo = fs.readFileSync("lib/foo.js").toString()
+
+foo = foo.replace(/export const/g, "const")
+foo = foo.replace(/export function/g, "function")
+foo = foo.replace(/export class/g, "class")
+
+fs.writeFileSync("lib/fooweb.js", foo)
